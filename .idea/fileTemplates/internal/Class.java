483b19db58902f1e1080034ea51c7e791caccb86
Ken Stanley <dohpaz@gmail.com>
2022-10-03 21:35:56 -0400

    Add template to ensure file header

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+// Group22_InClass08
+// ${NAME}.java
+// Ken Stanley & Stephanie Karp
+
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public class ${NAME} {
+}
